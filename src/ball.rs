d000cc954ef5cac96250657b88ea8fac10898731
Niwol <lowin000@gmail.com>
2022-05-08 09:50:10 +0200

    Verlet functional

---
+++ src/ball.rs
@@ -0,0 +1,62 @@
+use bevy::ecs::component::Component;
+use bevy::math::Vec2;
+
+#[derive(Component, Default)]
+pub struct Ball {
+    current_pos: Vec2,
+    prev_pos: Vec2,
+    acceleration: Vec2,
+    radius: f32,
+}
+
+impl Ball {
+    pub fn new(pos: Vec2, r: f32, vel: Vec2) -> Self {
+        Ball {
+            current_pos: pos,
+            prev_pos: -vel,
+            radius: r,
+            ..Default::default()
+        }
+    }
+
+    pub fn accelerate(&mut self, acc: Vec2) {
+        self.acceleration += acc;
+    }
+
+    pub fn update_position(&mut self, dt: f32) {
+        let vel = self.current_pos - self.prev_pos;
+        self.prev_pos = self.current_pos;
+        self.current_pos = self.current_pos + vel + self.acceleration * dt * dt;
+        self.acceleration = Vec2::new(0.0, 0.0);
+    }
+
+    pub fn apply_constraints(&mut self, _w: f32, _h: f32) {
+        let center = Vec2::new(0.0, 0.0);
+        let radius = 300.0;
+
+        let to_obj = self.current_pos - center;
+        let dist = to_obj.length();
+
+        if dist > radius - self.radius {
+            let n = to_obj.normalize();
+            self.current_pos = center + n * (radius - self.radius);
+        }
+    }
+
+    pub fn solve_colision(&mut self, other: &mut Self) {
+        let colision_axes = self.current_pos - other.current_pos;
+        let dist = colision_axes.length();
+        let min_dist = self.radius + other.radius;
+
+        if dist < min_dist {
+            let n = colision_axes.normalize();
+            let delta = min_dist - dist;
+            self.current_pos += 0.5 * delta * n;
+            other.current_pos -= 0.5 * delta * n;
+        }
+    }
+
+    pub fn get_position(&self) -> Vec2 {
+        self.current_pos
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,20 @@
-use bevy::{
-    prelude::*,
-    reflect::TypeUuid,
-    render::mesh::PrimitiveTopology,
-    sprite::{MaterialMesh2dBundle, Mesh2dHandle},
-};
+mod ball;
+
+use ball::Ball;
+use bevy::{prelude::*, render::mesh::PrimitiveTopology};
+use rand::Rng;
 
 pub struct VerletPlugin;
 
+struct TotalBalls(u64);
+
 impl Plugin for VerletPlugin {
     fn build(&self, app: &mut App) {
-        app.add_system(spawn_ball).add_system(update_balls);
+        app.insert_resource(TotalBalls(0))
+            .add_system(spawn_ball)
+            .add_system(remove_ball)
+            .add_system(update_balls);
     }
-}
-
-#[derive(Component, Default)]
-pub struct Ball {
-    x: f32,
-    y: f32,
-}
-
-#[derive(Default, Component)]
-pub struct CustomColor {
-    color: Color,
 }
 
 fn spawn_ball(
@@ -29,59 +22,124 @@ fn spawn_ball(
     input: Res<Input<KeyCode>>,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
+    mut total_balls: ResMut<TotalBalls>,
 ) {
-    if input.just_pressed(KeyCode::Space) {
-        let mut m = Mesh::new(PrimitiveTopology::TriangleList);
+    if !input.pressed(KeyCode::Space) && !input.just_pressed(KeyCode::X) {
+        return;
+    }
 
-        let mut vertices = Vec::new();
-        let mut normals = Vec::new();
-        let mut colors = Vec::new();
-        let mut indices = Vec::new();
+    let r = rand::thread_rng().gen_range(5.0..15.0);
+    let vel = Vec2::new(
+        rand::thread_rng().gen_range(-2.0..=2.0),
+        rand::thread_rng().gen_range(0.0..=1.0),
+    );
 
-        for i in 0..=20 {
-            let di = (i as f32 / 20.0) * 2.0 * 3.1415;
+    let color = Color::rgb(
+        rand::thread_rng().gen_range(0.0..=1.0),
+        rand::thread_rng().gen_range(0.0..=1.0),
+        rand::thread_rng().gen_range(0.0..=1.0),
+    );
 
-            let x = di.cos();
-            let y = di.sin();
+    let mut m = Mesh::new(PrimitiveTopology::TriangleList);
+    let mut positions = Vec::new();
+    let mut normals = Vec::new();
+    let mut uvs = Vec::new();
+    let mut indices = Vec::new();
 
-            let r = (x * 255.0) as u32;
-            let g = (y * 255.0) as u32;
-            let b = (((x + y) / 2.0) * 255.0) as u32;
-            let c = (r << (8 * 3)) + (g << (8 * 2)) + (b << (8 * 1)) + (0x000000ff);
-            // println!("{{ {}, {}, {} }}", r, g, b);
+    // Vertices
+    for i in 0..=20 {
+        let di = (i as f32 / 20.0) * 2.0 * 3.1415;
 
-            vertices.push([x, y, 0.0]);
-            normals.push([x, y, 0.0]);
-            colors.push(c);
+        let x = di.cos() * r;
+        let y = di.sin() * r;
+
+        positions.push([x, y, 0.0]);
+        normals.push([x, y, 0.0]);
+        uvs.push([x, y]);
+    }
+
+    // Indices
+    for i in 1..20 {
+        indices.push(0);
+        indices.push(i);
+        indices.push(i + 1);
+    }
+    let indices = bevy::render::mesh::Indices::U32(indices);
+
+    m.insert_attribute(Mesh::ATTRIBUTE_POSITION, positions);
+    m.insert_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
+    m.insert_attribute(Mesh::ATTRIBUTE_UV_0, uvs);
+    m.set_indices(Some(indices));
+
+    let mesh_bundle = ColorMesh2dBundle {
+        mesh: meshes.add(m).into(),
+        material: materials.add(ColorMaterial::from(color)),
+        ..default()
+    };
+
+    commands
+        .spawn_bundle(mesh_bundle)
+        .insert(Ball::new(Vec2::new(0.0, 0.0), r, vel));
+
+    total_balls.0 += 1;
+    println!("Total balls: {}", total_balls.0);
+}
+
+fn remove_ball(mut commands: Commands, query: Query<(Entity, &Ball)>) {
+    for (e, b) in query.iter() {
+        if b.get_position().y < -400.0 {
+            commands.entity(e).despawn();
         }
-
-        for i in 1..20 {
-            indices.push(0);
-            indices.push(i);
-            indices.push(i + 1);
-        }
-        let indices = bevy::render::mesh::Indices::U16(indices);
-
-        m.insert_attribute(Mesh::ATTRIBUTE_POSITION, vertices);
-        m.insert_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
-        m.insert_attribute(Mesh::ATTRIBUTE_COLOR, colors);
-        m.insert_attribute(Mesh::ATTRIBUTE_UV_0, None);
-        m.set_indices(Some(indices));
-
-        let mesh_bundle = ColorMesh2dBundle {
-            mesh: meshes.add(m).into(),
-            material: materials.add(ColorMaterial::from(Color::BLUE)),
-            ..default()
-        };
-
-        commands
-            .spawn_bundle(mesh_bundle)
-            .insert(Ball { ..default() });
     }
 }
 
-fn update_balls(time: Res<Time>, mut query: Query<&mut Transform, With<Ball>>) {
-    for mut t in query.iter_mut() {
-        t.translation.y -= 50.0 * time.delta_seconds();
+fn update_balls(
+    windows: Res<Windows>,
+    time: Res<Time>,
+    mut query: Query<(&mut Transform, &mut Ball)>,
+) {
+    let window = windows.get_primary().unwrap();
+    let dt = time.delta_seconds();
+
+    let sub_steps = 8;
+    let sub_dt = dt / sub_steps as f32;
+
+    for _ in 0..sub_steps {
+        apply_gravity(&mut query);
+        update_positions(sub_dt, &mut query);
+        apply_constraints(window, &mut query);
+        solve_colision(&mut query);
+    }
+
+    for (mut t, b) in query.iter_mut() {
+        t.translation.x = b.get_position().x;
+        t.translation.y = b.get_position().y;
+    }
+}
+
+fn apply_gravity(query: &mut Query<(&mut Transform, &mut Ball)>) {
+    let g = Vec2::new(0.0, -1000.0);
+
+    for (_, mut b) in query.iter_mut() {
+        b.accelerate(g);
+    }
+}
+fn apply_constraints(window: &Window, query: &mut Query<(&mut Transform, &mut Ball)>) {
+    for (_, mut b) in query.iter_mut() {
+        b.apply_constraints(window.width(), window.height());
+    }
+}
+
+fn solve_colision(query: &mut Query<(&mut Transform, &mut Ball)>) {
+    let mut iterator = query.iter_combinations_mut();
+
+    while let Some([(_, mut b1), (_, mut b2)]) = iterator.fetch_next() {
+        b1.solve_colision(&mut b2);
+    }
+}
+
+fn update_positions(dt: f32, query: &mut Query<(&mut Transform, &mut Ball)>) {
+    for (_, mut b) in query.iter_mut() {
+        b.update_position(dt);
     }
 }
